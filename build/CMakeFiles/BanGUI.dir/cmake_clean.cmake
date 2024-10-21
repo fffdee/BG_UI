139072e5fcdc16bda92db269d49bcdd615aa6a4d
BanGO <695371519@qq.com>
2024-10-21 22:11:46 +0800

    Init

---
+++ build/CMakeFiles/BanGUI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BanGUI"
+  "BanGUI.pdb"
+  "CMakeFiles/BanGUI.dir/src/main.c.o"
+  "CMakeFiles/BanGUI.dir/src/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BanGUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/BanGUI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BanGUI.
+# This may be replaced when dependencies are built.
